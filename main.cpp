f79bb48f0b1b60d60519fd6eecf05a0133af6695
Semen Tsokurenko <semka0tsocurenco@gmail.com>
2025-04-18 11:52:05 +0300

    Добавлена перегрузка оператора вывода + сам вывод

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 #include <chrono>
 #include <vector>
 #include <cmath>
-#include <memory>      
+#include <memory>    
+#include <random>  
+#include <iomanip>  
 
-#define DIMENTIONS 10
+#define DIMENSIONS 10
 #define N_PARTICLES 100
 #define MIN_RAND -10
 #define MAX_RAND 10
 
 
-double random(double min, double max)
-{
-    return (double)(rand())/RAND_MAX*(max - min) + min; 
-}
 
+double random(double lo, double hi) {
+    static std::mt19937_64 rng{std::random_device{}()};
+    std::uniform_real_distribution<double> dist(lo, hi);
+    return dist(rng);
+}
 
 double target_function (std::vector<double> possitions) {
     double sum;
@@ -26,38 +29,42 @@ double target_function (std::vector<double> possitions) {
 }
 
 
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v){
+    os<< "[ ";
+    for (int i= 0 ; i < v.size(); i++){
+        os<<"\33[32m"<<v[i]<<"\33[0m";
+        if (i + 1 < v.size()) 
+            os << ", ";
+    }
+    os<< " ]";
+    return os;
+}
+
 
 
 int main() {
-    std::srand(time(NULL));
 
-    std::vector<std::vector<double>> possiions (N_PARTICLES, std::vector<double>(DIMENTIONS));
-    std::vector<std::vector<double>> velocities (N_PARTICLES, std::vector<double>(DIMENTIONS));
+    std::vector<std::vector<double>> positions (N_PARTICLES, std::vector<double>(DIMENSIONS));
+    std::vector<std::vector<double>> velocities (N_PARTICLES, std::vector<double>(DIMENSIONS, 0.0));
 
-    for (int N = 0 ; N < N_PARTICLES ; N++){
-        for (int D = 0 ; D < DIMENTIONS ; D++){     
-            possiions[N][D] = random(MIN_RAND, MAX_RAND);
-            velocities[N][D] = 0.0;
-        }
+    for (int i = 0; i < N_PARTICLES; ++i)
+        for (int d = 0; d < DIMENSIONS; ++d)
+            positions[i][d] = random(MIN_RAND, MAX_RAND);
+
+    for (int i = 0; i < N_PARTICLES; ++i) {
+        std::cout << std::fixed << std::setprecision(3)<< std::showpos;
+        std::cout << "\33[34mParticle " << i << ":\33[0m\n";
+        std::cout << "  pos: " << positions[i] << "\n";
+        std::cout << "  vel: " << velocities[i] << "\n\n";
     }
+
+
 
 
 
     std::unique_ptr<double> global_best_ptr;
     double global_best_fitness2 = std::numeric_limits<double>::infinity();
-
-
-
-
-
-    std::cout<<"\nAA\n";
-
-
-
-    // for (int i = 30 ;;){
-    //     std::cout<<"\r\33["<<std::to_string(i)<<"m Hello World!"<<std::flush;
-    //     i = (i > 35) ? (30) : (i + 1);
-    // }
 
     return 0;
 }
